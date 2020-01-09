dec8f3e199118cbc6dd54638b7db649f7b113481
Christian Wacker <christianwwacker@gmail.com>
2020-01-09 19:29:25 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,17 +1,16 @@
-function callMe() {
+var callMe = function() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
 
+crazy();
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
